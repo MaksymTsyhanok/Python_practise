7faeb9f42be1b522866e060ad31b59c0b02beb64
MaksymTsyhanok <149295280+MaksymTsyhanok@users.noreply.github.com>
2024-01-13 13:18:32 +0000

    calc_modul_2

--- Modul_1/main.py
+++ Modul_1/main.py
@@ -1,3 +1,12 @@
-print('Hello World')
-print(1+2)
-print([1, 2, 3])
+number = 10 # int незмінюванний тип даних
+number_2 = 10.5123 # float незмінюванний тип даних
+name = 'Bob' # str незмінюванний тип даних
+is_activ = True # bool незмінюванний тип даних
+vare = None # NoneType незмінюванний тип даних
+cars = [1, 2, 3, 4, 'bmw', True, False, None] # list змінюванний тип данних
+books = {1:'math', 2:'history', 'bmw':5, 'active':True, 3:cars} # dict змінюванний тип данних; ключ це завжди незмінюванний тип данних (int, float, str, tuple)
+points = (5, 9) #tuple незмінюванний тип даних
+colurs = {'black', 'white', 'ellouw', 'black'} # set змінюванний тип данних. Це список, але збергає лише унікальні елементи
+print(colurs)
+colurs.add('green')
+print(colurs)
